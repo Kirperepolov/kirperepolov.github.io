5da4656cb27eaee7b590a7631c8caa1a4f16b424
Kir Perepolov <johnyd@bigmir.net>
2017-01-30 17:35:31 +0200

    some content about me

--- src/common/routes.js
+++ src/common/routes.js
@@ -8,12 +8,12 @@
   function RoutesConfig($stateProvider, $urlRouterProvider){
 
     // Redirect to tab 1 if no other URL matches
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('');
 
     // Set up UI states
     $stateProvider
       .state('home',{
-        url:'/',
+        url:'',
         templateUrl:"src/home/home.template.html"
       });
 
