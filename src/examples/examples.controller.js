63495cf79fea24982f222814b37294c9d9c71e55
Kir Perepolov <johnyd@bigmir.net>
2017-02-04 18:32:24 +0200

    edited styles

--- src/examples/examples.controller.js
+++ src/examples/examples.controller.js
@@ -8,8 +8,23 @@
   function ExamplesController(examples){
     var exmpCtrl = this;
     exmpCtrl.examples = examples;
+    let examplesQuantity = exmpCtrl.examples.length;
 
-    
+    let activeSlide = 0;
+
+    exmpCtrl.whattheClass = function(index){
+      if (activeSlide===index){
+        return "active";
+      } else if (activeSlide===(index-1+examplesQuantity)%examplesQuantity) {
+        return "next";
+      } else if (activeSlide===(index+1+examplesQuantity)%examplesQuantity) {
+        return "previous";
+      } else {return "hidden"};
+    };
+
+    exmpCtrl.activate = function(index){
+      activeSlide = index;
+    };
 
   };
 }());
